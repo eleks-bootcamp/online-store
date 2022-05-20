5bd168352b0ee9b252943d23fad869040923b6d2
Taras <102380292+TarasEdv@users.noreply.github.com>
2022-05-20 17:32:35 +0300

    Components uodate

---
+++ double-slider/js.js
@@ -0,0 +1,32 @@
+var lowerSlider = document.querySelector('#lower'),
+   upperSlider = document.querySelector('#upper'),
+   lowerVal = parseInt(lowerSlider.value);
+   upperVal = parseInt(upperSlider.value);
+
+upperSlider.oninput = function() {
+   lowerVal = parseInt(lowerSlider.value);
+   upperVal = parseInt(upperSlider.value);
+
+   if (upperVal < lowerVal + 4) {
+      lowerSlider.value = upperVal - 4;
+
+      if (lowerVal == lowerSlider.min) {
+         upperSlider.value = 4;
+      }
+   }
+};
+
+
+lowerSlider.oninput = function() {
+   lowerVal = parseInt(lowerSlider.value);
+   upperVal = parseInt(upperSlider.value);
+
+   if (lowerVal > upperVal - 4) {
+      upperSlider.value = lowerVal + 4;
+
+      if (upperVal == upperSlider.max) {
+         lowerSlider.value = parseInt(upperSlider.max) - 4;
+      }
+
+   }
+};
