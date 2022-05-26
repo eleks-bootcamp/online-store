c1d825dc062caa2658f0bbb7ec3c8569c4149bc2
VLK2021 <volodymyrkostiuk2021@gmail.com>
2022-05-26 13:04:12 +0300

    online-store Volodymyr_Kostiuk JS cards-list

--- src/components/cards-list/02-js-task/index.js
+++ src/components/cards-list/02-js-task/index.js
@@ -1,3 +1,134 @@
 export default class CardsList {
+  constructor(products) {
+    this.element = products;
+
+    this.render();
+  }
+
+  getT() {
+    return `
+    <div class="wrapper-component">
+      <div class="os-img">
+        <img src="../01-css-task/images/Rectangle.png" alt="foto">
+      </div>
+      <div class="os-price">
+        <div class="os-price-star">
+          <p class="size">1.23</p>
+          <img src="../01-css-task/images/Star.png" alt="icon">
+        </div>
+
+        <p class="os-price-price size">
+          15999
+        </p>
+      </div>
+      <div class="os-text">
+        <p class="size">Ноутбук Acer Aspire 3 A315-57G-336G <br> (NX.HZREU.01S) Charcoal Black</p>
+        <p class="os-text-two size">laptops</p>
+      </div>
+      <div class="os-footer size">
+        <p>add to cart</p>
+      </div>
+    </div>
+    <div class="wrapper-component">
+      <div class="os-img">
+        <img src="../01-css-task/images/Rectangle.png" alt="foto">
+      </div>
+      <div class="os-price">
+        <div class="os-price-star">
+          <p class="size">2.42</p>
+          <img src="../01-css-task/images/Star.png" alt="icon">
+
+        </div>
+
+        <p class="os-price-price size">
+          15999
+        </p>
+      </div>
+      <div class="os-text">
+        <p class="size">Ноутбук Acer Aspire 3 A315-57G-336G <br> (NX.HZREU.01S) Charcoal Black</p>
+        <p class="os-text-two size">laptops</p>
+      </div>
+      <div class="os-footer size">
+        <p>add to cart</p>
+      </div>
+    </div>
+    <div class="wrapper-component">
+      <div class="os-img">
+        <img src="../01-css-task/images/Rectangle.png" alt="foto">
+      </div>
+      <div class="os-price">
+        <div class="os-price-star">
+          <p class="size">1.96</p>
+          <img src="../01-css-task/images/Star.png" alt="icon">
+
+        </div>
+
+        <p class="os-price-price size">
+          15999
+        </p>
+      </div>
+      <div class="os-text">
+        <p class="size">Ноутбук Acer Aspire 3 A315-57G-336G <br> (NX.HZREU.01S) Charcoal Black</p>
+        <p class="os-text-two size">laptops</p>
+      </div>
+      <div class="os-footer size">
+        <p>add to cart</p>
+      </div>
+    </div>
+    <div class="wrapper-component">
+      <div class="os-img">
+        <img src="../01-css-task/images/Rectangle.png" alt="foto">
+      </div>
+      <div class="os-price">
+        <div class="os-price-star">
+          <p class="size">1.15</p>
+          <img src="../01-css-task/images/Star.png" alt="icon">
+
+        </div>
+
+        <p class="os-price-price size">
+          15999
+        </p>
+      </div>
+      <div class="os-text">
+        <p class="size">Ноутбук Acer Aspire 3 A315-57G-336G <br> (NX.HZREU.01S) Charcoal Black</p>
+        <p class="os-text-two size">laptops</p>
+      </div>
+      <div class="os-footer size">
+        <p>add to cart</p>
+      </div>
+    </div>
+    <div class="wrapper-component">
+      <div class="os-img">
+        <img src="../01-css-task/images/Rectangle.png" alt="foto">
+      </div>
+      <div class="os-price">
+        <div class="os-price-star">
+          <p class="size">1.45</p>
+          <img src="../01-css-task/images/Star.png" alt="icon">
+
+        </div>
+
+        <p class="os-price-price size">
+          15999
+        </p>
+      </div>
+      <div class="os-text">
+        <p class="size">Ноутбук Acer Aspire 3 A315-57G-336G <br> (NX.HZREU.01S) Charcoal Black</p>
+        <p class="os-text-two size">laptops</p>
+      </div>
+      <div class="os-footer size">
+        <p>add to cart</p>
+      </div>
+    </div>
+    `
+  }
+
+  render() {
+    const block = document.createElement('div');
+    block.classList = 'block';
+    block.innerHTML = this.getT();
+    this.element = block;
+  }
 
 }
