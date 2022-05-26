4833cc3b07649ceda4c1fdf301912e8c93f4c3df
Mykola <mykola.lwiv@gmail.com>
2022-05-26 20:17:35 +0300

    add card

--- src/components/card/02-js-task/index.js
+++ src/components/card/02-js-task/index.js
@@ -1,3 +1,34 @@
 export default class Card {
+constructor(someProduct) {
+  this.product=someProduct;
+  this.HTMLForm();
 
+}
+HTMLForm(){
+let elements= document.createElement('div');
+  elements.className = "card-shop";
+elements.innerHTML=`
+  <div class="card-shop-main">
+    <div class="card-shop-img">
+      <img src=${this.product.images[1]} alt="">
+    </div>
+    <div class="card-shop-price-and-mark">
+      <div class="card-shop-mark">
+        <p>${this.product.rating}</p>
+        <i class="bi bi-star"></i>
+      </div>
+      <p class="card-shop-price">${this.product.price}</p>
+    </div>
+    <div class="card-shop-name">
+      <a href="#">${this.product.title}</a>
+      <p>${this.product.category}</p>
+    </div>
+
+  </div>
+  <button class="card-shop-button">
+    ADD TO CART
+  </button>
+    `
+  this.componentElement=elements;
+}
 }
---
+++ src/components/cards-list/02-js-task/prodact-list.js
@@ -0,0 +1,76 @@
+(function (){
+  const productJson = `[
+    {"id": "76w0hz7015kkr9qqqqq",
+      "images": [
+        "https://content2.rozetka.com.ua/goods/images/big_tile/163399632.jpg",
+        "https://content.rozetka.com.ua/goods/images/big_tile/163399633.jpg"
+      ],
+      "title": "Ноутбук Acer Aspire 3 A315-57G-336G (NX.HZREU.01S) Charcoal Black",
+      "rating": 2.89,
+      "price": 15999,
+      "category": "laptops",
+      "brand": "acer"};
+      {"id": "76w0hz7015kkr9eewwq",
+      "images": [
+        "https://content2.rozetka.com.ua/goods/images/big_tile/163399632.jpg",
+        "https://content.rozetka.com.ua/goods/images/big_tile/163399633.jpg"
+      ],
+      "title": "Ноутбук Acer Aspire 3 A315-57G-336G (NX.HZREU.01S) Charcoal Black",
+      "rating": 1.89,
+      "price": 15999,
+      "category": "laptops",
+      "brand": "acer"};
+      {"id": "76w0hz7015kkr9qqwwe",
+      "images": [
+        "https://content2.rozetka.com.ua/goods/images/big_tile/163399632.jpg",
+        "https://content.rozetka.com.ua/goods/images/big_tile/163399633.jpg"
+      ],
+      "title": "Ноутбук Acer Aspire 3 A315-57G-336G (NX.HZREU.01S) Charcoal Black",
+      "rating": 3.89,
+      "price": 15999,
+      "category": "laptops",
+      "brand": "acer"};
+      {"id": "76w0hz7015kkr9kjhgd",
+      "images": [
+        "https://content2.rozetka.com.ua/goods/images/big_tile/163399632.jpg",
+        "https://content.rozetka.com.ua/goods/images/big_tile/163399633.jpg"
+      ],
+      "title": "Ноутбук Acer Aspire 3 A315-57G-336G (NX.HZREU.01S) Charcoal Black",
+      "rating": 4.89,
+      "price": 15999,
+      "category": "laptops",
+      "brand": "acer"};
+      {"id": "76w0hz7015kkr9qweer",
+      "images": [
+        "https://content2.rozetka.com.ua/goods/images/big_tile/163399632.jpg",
+        "https://content.rozetka.com.ua/goods/images/big_tile/163399633.jpg"
+      ],
+      "title": "Ноутбук Acer Aspire 3 A315-57G-336G (NX.HZREU.01S) Charcoal Black",
+      "rating": 1.22,
+      "price": 15999,
+      "category": "laptops",
+      "brand": "acer"};
+      {"id": "76w0hz7015kkr9tertg",
+      "images": [
+        "https://content2.rozetka.com.ua/goods/images/big_tile/163399632.jpg",
+        "https://content.rozetka.com.ua/goods/images/big_tile/163399633.jpg"
+      ],
+      "title": "Ноутбук Acer Aspire 3 A315-57G-336G (NX.HZREU.01S) Charcoal Black",
+      "rating": 2.31,
+      "price": 15999,
+      "category": "laptops",
+      "brand": "acer"};
+      {"id": "76w0hz7015kkr9fdsae",
+      "images": [
+        "https://content2.rozetka.com.ua/goods/images/big_tile/163399632.jpg",
+        "https://content.rozetka.com.ua/goods/images/big_tile/163399633.jpg"
+      ],
+      "title": "Ноутбук Acer Aspire 3 A315-57G-336G (NX.HZREU.01S) Charcoal Black",
+      "rating": 4.11,
+      "price": 15999,
+      "category": "laptops",
+      "brand": "acer"};
+
+  ]`;
+  const products = JSON.parse(productJson);
+})();
