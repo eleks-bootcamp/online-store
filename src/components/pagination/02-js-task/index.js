205abcbf451754b1e4312eec0889fbe34c69d492
volodymyr35 <vzhukevych@gmail.com>
2022-05-26 21:51:17 +0300

    added new files to pagination-component-js-task

--- src/components/pagination/02-js-task/index.js
+++ src/components/pagination/02-js-task/index.js
@@ -1,3 +1,33 @@
 export default class Pagination {
+  element;
+  start = 0;
+  pageIndex = 0;
 
+  constructor({
+    totalPages = 10,
+    page = 1,
+  } = {}) {
+    this.totalPages = totalPages;
+    this.pageIndex = page - 1;
+
+  }
+
+    goToPrevPage () {
+      // ... your logic
+    }
+
+    goToNextPage () {
+      // ... your logic
+    }
+
+    remove () {
+      if (this.elemnt) {
+        this.element.remove();
+      }
+    }
+
+    destroy () {
+      this.remove();
+      this.element = null;
+    }
 }
