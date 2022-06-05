e8c7acf04fe8513cd9d28cec08967682dfb418f5
SDubytskiy <85970406+SDubytskiy@users.noreply.github.com>
2022-06-05 15:29:31 +0200

    add js card

--- src/components/card/02-js-task/index.js
+++ src/components/card/02-js-task/index.js
@@ -1,3 +1,19 @@
-export default class Card {
+// export default 
+// const title = "Ноутбук Acer Aspire 3 A315-57G-336G (NX.HZREU.01S) Charcoal Black";
+// console.log('hello')
 
-}
+// class Card {
+//     constructor (omeTitle) {
+//         this.componentTitle = someTitle;
+
+//         console.log('this.componentTitle', this.componentTitle);
+
+//     }
+
+//     myRender () {
+//         const element = document.createElement('div');
+//         <div>
+//         <div>some description of product here..</div>
+//         </div>
+//     }
+// }
