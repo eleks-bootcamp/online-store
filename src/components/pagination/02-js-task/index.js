402d97655d28133f6cca277a32dbe26e05ecee7a
volodymyr35 <vzhukevych@gmail.com>
2022-06-05 23:29:27 +0300

    added logic to pagination-componenet-js

--- src/components/pagination/02-js-task/index.js
+++ src/components/pagination/02-js-task/index.js
@@ -1,17 +1,22 @@
 export default class Pagination {
   element;
 
-  constructor({ totalPages = 10, page = 1 } = {}) {
+  constructor({ totalPages = 12, activePageIndex = 0 } = {}) {
     this.totalPages = totalPages;
-    this.pageIndex = page - 1;
+    this.activePageIndex = activePageIndex;
 
     this.render();
+    this.addEventListeners();
   }
 
-  renderPageItem(number) {
+  renderPageItem(pageIndex = 0) {
+    const isActive = pageIndex === this.activePageIndex ? "active" : "";
+
     return `
       <li>
-        <a href="#">${number}</a>
+        <a href="#" data-element="page-link" class="page-link ${isActive}" data-page-index="${pageIndex}">
+        ${pageIndex + 1}
+        </a>
       </li>
     `;
   }
@@ -22,15 +27,54 @@ export default class Pagination {
     //   output += this.renderPageItem(i);
     // }
     // return output;
-    return new Array(this.totalPages).fill(null).map((_, i) => this.renderPageItem(i + 1)).join('');
+    return new Array(this.totalPages)
+      .fill(null)
+      .map((_, i) => this.renderPageItem(i))
+      .join("");
   }
 
   get template() {
     return `
-      <a href="#"><img src="icons/chevron-left.svg"></a>
-      <ul class="pagination__options">${this.getPages()}</ul>
-      <a href="#"><img src="icons/chevron-right.svg"></a>
+      <a href="#" data-element="nav-prev"><img src="icons/chevron-left.svg"></a>
+      <ul data-element="pagination" class="pagination__options">${this.getPages()}</ul>
+      <a href="#" data-element="nav-next"><img src="icons/chevron-right.svg"></a>
     `;
+  }
+
+  setPage(pageIndex = 0) {
+    if (pageIndex === this.activePageIndex) return;
+    if (pageIndex > this.totalPages - 1 || pageIndex < 0) return;
+
+    const activePage = this.element.querySelector(".page-link.active");
+
+    if (activePage) {
+      activePage.classList.remove("active");
+    }
+
+    const nextActivePage = this.element.querySelector(
+      `[data-page-index="${pageIndex}"]`
+    );
+
+    if (nextActivePage) {
+      console.log(
+        this.element.querySelectorAll('[data-element="page-link"]')[pageIndex]
+      );
+      this.element
+        .querySelectorAll('[data-element="page-link"]')
+        [pageIndex].classList.add("active");
+    }
+
+    this.activePageIndex = pageIndex;
+  }
+
+  goToPrevPage() {
+    const prevPageIndex = this.activePageIndex - 1;
+    this.setPage(prevPageIndex);
+  }
+
+  goToNextPage() {
+    const nextPageIndex = this.activePageIndex + 1;
+    this.setPage(nextPageIndex);
   }
 
   render() {
@@ -42,12 +86,28 @@ export default class Pagination {
     this.element = wrapper;
   }
 
-  goToPrevPage() {
-    // ... your logic
-  }
+  addEventListeners() {
+    const prevPageBtn = this.element.querySelector('[data-element="nav-prev"]');
+    const nextPageBtn = this.element.querySelector('[data-element="nav-next"]');
+    const pagesList = this.element.querySelector('[data-element="pagination"]');
 
-  goToNextPage() {
-    // ... your logic
+    prevPageBtn.addEventListener("click", () => {
+      this.goToPrevPage();
+    });
+
+    nextPageBtn.addEventListener("click", () => {
+      this.goToNextPage();
+    });
+
+    pagesList.addEventListener("click", (event) => {
+      const pageItem = event.target.closest(".page-link");
+
+      if (!pageItem) return;
+
+      const { pageIndex } = pageItem.dataset;
+
+      this.setPage(parseInt(pageIndex, 10));
+    });
   }
 
   remove() {
