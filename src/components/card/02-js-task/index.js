3a7ed3c64cd5a4a26aec75a10975067a6602325e
Alina2Kl <vivernaalik87@gmail.com>
2022-06-05 15:44:12 +0300

    js-card-1

--- src/components/card/02-js-task/index.js
+++ src/components/card/02-js-task/index.js
@@ -1,3 +1,44 @@
 export default class Card {
+    constructor (someProduct) {
+      this.state = someProduct;
+      this.myRender();
+    }
 
-}
+    getTemplate (){ 
+      return `
+        <div class="card_container">
+            <div class="card_item-photo" style="background-image: url(${this.state.images})">
+            </div>
+            <div class="card_descriptin">
+                <div class="card-item_pr-rt">
+                    <div class="card_rate">
+                    <span>${this.state.rating}</span>
+                    <i class="bi bi-star"></i>
+                    </div>
+                    <div class="card_price">${this.state.price}</div>
+                </div>
+                <h5 class="card_title">${this.state.title}</h5>
+                <p class="card_category">${this.state.category}</p>
+            </div>
+            <div class="card_footer">
+                <button>
+                ADD TO CART
+                </button>
+            </div>
+        </div>
+        `;
+
+        return result;
+      }
+
+      update(data = {}) {
+        this.state = data;
+        this.componentElement.innerHTML = this.getTemplate();
+      }
+
+      myRender () {
+        const element = document.createElement('div');
+        element.innerHTML = this.getTemplate();
+        this.componentElement = element;
+      }
+    }
