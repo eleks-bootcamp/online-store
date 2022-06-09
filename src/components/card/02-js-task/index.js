fc6886a474b43c934ffa4f18dc008df5d24a96d5
Volodymyr Shevchuk <dosandkv@gmail.com>
2022-06-09 18:11:55 +0300

    Added "page-component", added "advanced" pagination component;

--- src/components/card/02-js-task/index.js
+++ src/components/card/02-js-task/index.js
@@ -1,49 +1,65 @@
-export default class Card {
-  constructor (someProduct) {
-    this.state = someProduct;
-    this.myRender();
+import Card from "./card.js";
+import Pagination from "./pagination.js";
+
+const product = {
+  id: "76w0hz7015kkr9kjkav",
+  images: [
+    "https://content2.rozetka.com.ua/goods/images/big_tile/163399632.jpg",
+    "https://content.rozetka.com.ua/goods/images/big_tile/163399633.jpg",
+  ],
+  title:
+    "Ноутбук Acer Aspire 3 A315-57G-336G (NX.HZREU.01S) Charcoal Black",
+  rating: 2.89,
+  price: 15999,
+  category: "laptops",
+  brand: "acer",
+};
+
+export default class OnlineStorePage {
+  constructor () {
+    this.components = {};
+
+    this.initComponents();
+    this.render();
+    this.renderComponents();
   }
 
   getTemplate () {
-    const result =  `
-      <div class="os-product-card">
-        <div class="os-product-img" style="background-image: url(${this.state.images[0]});"></div>
-
-        <div class="os-product-content">
-          <div class="os-product-price-wrapper">
-            <div class="os-product-rating">
-              <span>${this.state.rating}</span>
-              <i class="bi bi-star"></i>
-            </div>
-
-            <div class="os-product-price">${this.state.price}</div>
-          </div>
-
-          <h5 class="os-product-title">${this.state.title}</h5>
-          <p class="os-product-description">${this.state.category}</p>
+    return `
+      <div>
+        <div data-element="card">
+          <!-- Card component -->
         </div>
-
-        <footer class="os-product-footer">
-          <button class="os-btn-primary" data-element="addToCartBtn">
-            Add To Cart
-          </button>
-        </footer>
+        <div data-element="pagination">
+          <!-- Pagination component -->
+        </div>
       </div>
     `;
-
-    return result
   }
 
-  update(data = {}) {
-    this.state = data;
-    this.componentElement.innerHTML = this.getTemplate();
+  initComponents () {
+    const card = new Card(product);
+    const pagination = new Pagination({
+      activePageIndex: 2
+    });
+
+    this.components.card = card;
+    this.components.pagination = pagination;
   }
 
-  myRender () {
+  renderComponents () {
+    const cardContainer = this.element.querySelector('[data-element="card"]');
+    const paginationContainer = this.element.querySelector('[data-element="pagination"]');
+
+    cardContainer.append(this.components.card.componentElement);
+    paginationContainer.append(this.components.pagination.element);
+  }
+
+  render () {
     const wrapper = document.createElement('div');
 
     wrapper.innerHTML = this.getTemplate();
 
-    this.componentElement = wrapper.firstElementChild;
+    this.element = wrapper.firstElementChild;
   }
 }
