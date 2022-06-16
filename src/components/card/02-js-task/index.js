db3d0ae2d00e74f9cfa0842cb710c98bfc9779fe
Misha <drohomiretskiy@gmail.com>
2022-06-16 20:26:18 +0300

    add component pagination and update component card

--- src/components/card/02-js-task/index.js
+++ src/components/card/02-js-task/index.js
@@ -1,19 +1,19 @@
 export default class Card {
-    constructor(someProduct) {
-      this.state = someProduct;
-      this.myRender();
-    }
-    getTemplate() {
-      return `
-      <div class="card">
-        <img class="product_photo" src="${this.state.images[0]}" alt="Фото товару">
-         <div class="product_content">
-          <span class="bloc_favorite">
-           <span class="counter">${this.state.rating}</span>
+  constructor(someProduct) {
+    this.state = someProduct;
+    this.myRender();
+  }
+  getTemplate() {
+    return `
+    <div class="card">
+    <img class="product_photo" src="${this.state.images[0]}" alt="Фото товару">
+      <div class="product_content">
+        <span class="bloc_favorite">
+          <span class="counter">${this.state.rating}</span>
           <span class="btn_favorite"><i class="bi bi-star"></i></span>
-         </span>
+        </span>
         <span class="price">${this.state.price}</span>
-        <div class="description">
+        <div class="title">
           <h5>${this.state.title}</h5>
         </div>
         <div class="category">
@@ -21,40 +21,20 @@ export default class Card {
         </div>
       </div>
       <button class="btn_addToCart">ADD TO CART</button>
-    </div>
-      `
-    }
-    update(data = {}) {
-      this.state = data;
-      this.componentElement.innerHTML = this.getTemplate();
-    }
-    myRender() {
-      const element = document.createElement('div');
-      element.innerHTML = this.getTemplate();
-      this.componentElement = element;
-    }
+  </div>
+  `;
+  }
+  update(data = {}) {
+    this.state = data;
+    this.componentElement.innerHTML = this.getTemplate();
 
+  }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  myRender() {
+    const element = document.createElement('div');
+    element.innerHTML = this.getTemplate();
+    this.componentElement = element;
+  }
 
 
 }
--- src/components/card/02-js-task/pagination.js
+++ src/components/card/02-js-task/pagination.js
@@ -1,43 +1,85 @@
 export default class Pagination {
-
-  constructor() {
-    this.defaultPagesSize = 12;
-    this.myRender();
+  defoultPagesSize = 20;
+  constructor({ activePageIndex = 0 } = {}) {
+    this.activePageIndex = activePageIndex;
+    this.render();
+    this.addEventListeners();
   }
   getTemplate() {
     return `
-   <section class="pagination">
-   <div class="btn-prev"> <a href="#"> <i class="bi bi-chevron-left"></i></a></div>
- ${this.getPages()}
-   <div class="btn-next"> <a href="#"><i class="bi bi-chevron-right"></i></a> </div>
- </section>
-
-   `;
+    <section class="pagination">
+    <div class="btn-prev" data-element="nav-prev"><a href="#" class="page-link"><i class="bi bi-chevron-left"></i></a></div>
+    <div class="btn-double-prev" data-element="nav-double-prev"><a href="#" class="page-link"><i class="bi bi-chevron-double-left"></i></a></div>
+    ${this.getPages()}
+    <div class="btn-double-next" data-element="nav-double-next"><a href="#" class="page-link"><i class="bi bi-chevron-double-right"></i></a></div>
+    <div class="btn-next" data-element="nav-next"><a href="#" class="page-link"><i class="bi bi-chevron-right"></i></a> </div>
+  </section>
+    `;
   }
   getPages() {
     return `
-  <ul>
-    ${this.getPageTamplate(this.defaultPagesSize)}
-  </ul>
-   `;
+    <ul class="pagination-list" data-element="pagination">
+      ${new Array(this.defoultPagesSize).fill(1).map((item, index) => {
+      return this.getPageTemplate(index);
+    }).join('')}
+    </ul>
+    `;
   }
-  getPageTamplate(pageIndex = 1) {
-    let str = '';
-    let allPages = '';
-    let i = 1;
-    do {
-      str = `<li><a href="#page${i}" >${i}</a></li>`;
-      allPages += str;
-      i++;
+  getPageTemplate(pageIndex = 0) {
+    const isActive = pageIndex === this.activePageIndex ? 'active' : '';
+    return `
+     <li class="pagination-item">
+      <a href="#" data-element="page-link"
+      class="page-link ${isActive}"data-page-index="${pageIndex}">${pageIndex + 1}</a>
+     </li>
+    `;
+  }
+  setPage(pageIndex = 0) {
+    if (pageIndex === this.activePageIndex) return;
+    if (pageIndex > this.defoultPagesSize - 1 || pageIndex < 0) return;
+    const activePage = this.element.querySelector('.page-link.active');
+    if (activePage) {
+      activePage.classList.remove('active');
     }
-    while (i <= pageIndex)
-    console.log(allPages);
-    return allPages;
+    const nextActivePage = this.element.querySelector(`[data-page-index="${pageIndex}"]`)
 
+    if (nextActivePage) {
+      nextActivePage.classList.add('active');
+    }
+    this.activePageIndex = pageIndex;
   }
-  myRender() {
-    const wrapper = document.createElement('div');
-    wrapper.innerHTML = this.getTemplate();
-    this.element = wrapper;
+  nextPage() {
+    const nextPageIndex = this.activePageIndex + 1;
+    this.setPage(nextPageIndex);
+  }
+  prevPage() {
+    const prevPageIndex = this.activePageIndex - 1;
+
+    this.setPage(prevPageIndex);
+  }
+  render() {
+    const wraper = document.createElement('div');
+    wraper.innerHTML = this.getTemplate();
+    this.element = wraper;
+  }
+  addEventListeners() {
+    const prevPageBtn = this.element.querySelector('[data-element="nav-prev"]');
+    const nextPageBtn = this.element.querySelector('[data-element="nav-next"]');
+    const pagesList = this.element.querySelector('[data-element="pagination"]');
+    prevPageBtn.addEventListener('click', ()=> {
+      this.prevPage();
+    });
+    nextPageBtn.addEventListener('click', () => {
+      this.nextPage();
+    });
+
+
+    pagesList.addEventListener('click', event => {
+      const pageItem = event.target.closest('.page-link');
+      if (!pageItem) return;
+      const{ pageIndex }= pageItem.dataset;
+      console.log(typeof pageIndex);
+      this.setPage(parseInt(pageIndex, 10));
+    });
   }
 }
