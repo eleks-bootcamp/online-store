2f1b299fa3b3797af9f53f2d7a7fb7ad3375c179
vlk2022 <volodymyrkostiuk2021@gmail.com>
2022-06-17 18:33:42 +0300

    feyk

---
+++ src/components/card/02-js-task/card.js
@@ -0,0 +1,47 @@
+export default class Card {
+  block;
+
+  constructor(someCard) {
+    this.state = someCard;
+    this.myRender();
+  }
+
+  getTemplate() {
+    return `<div class="wrap">
+        <div class="os-img">
+            <img src=${this.state.images[0]} alt="foto">
+        </div>
+
+        <div class="os-price">
+            <div class="os-price-star">
+                <p class="size">${this.state.rating}</p>
+            <img src="../01-css-task/images/Star.png" alt="icon">
+        </div>
+
+        <p class="os-price-price size">
+            ${this.state.price}
+        </p>
+    </div>
+
+        <div class="os-text">
+            <p class="size">${this.state.title}</p>
+            <p class="os-text-two size">${this.state.category}</p>
+        </div>
+
+        <div><button class="os-footer size">add to cart</button></div>
+    </div>`
+  }
+
+  update(data = {}) {
+    this.state = data;
+    this.element.innerHTML = this.getTemplate();
+  }
+
+  myRender() {
+    const block = document.createElement('div');
+    block.classList = "block";
+    block.innerHTML = this.getTemplate();
+
+    this.element = block.firstElementChild;
+  }
+}
