40dc2d3611c27fe2d73f8ed7989fe824ea3abfac
Oleksa <oleksa.antsupov@gmail.com>
2022-06-23 09:44:07 +0300

    add API in the index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@
 // import Card from "./components/product-card/card.js";
 import CardsList from "./components/cards-list/cards-list.js";
 import Pagination from "./components/pagination/pagination.js";
-import { products } from "./products.js";
+// import { products } from "./products.js";
 
+const BACKEND_URL = 'https://online-store.bootcamp.place/api/';
 class OnLineStorePage {
-  constructor(products) {
-    this.pageSize = 3;
-    this.products = products;
+  constructor() {
+    this.pageSize = 9;
+    this.products = [];
+
+    this.url = new URL('products', BACKEND_URL);
+    this.url.searchParams.set('_limit', this.pageSize);
+
     this.components = {};
 
     this.initComponents();
@@ -16,6 +21,8 @@ class OnLineStorePage {
     this.renderComponents();
 
     this.initEventListeners();
+
+    this.update(1);
   }
 
   getTemplate() {
@@ -28,9 +35,11 @@ class OnLineStorePage {
   }
 
   initComponents() {
-    const totalPages = Math.ceil(this.products.length / this.pageSize);
+    // TODO: remove hardcoded value
+    const totalElements = 100;
+    const totalPages = Math.ceil(totalElements/ this.pageSize);
 
-    const cardsList = new CardsList(this.products.slice(0, this.pageSize));
+    const cardsList = new CardsList(this.products);
     const pagination = new Pagination({
       activePageIndex: 0,
       totalPages
@@ -60,15 +69,27 @@ class OnLineStorePage {
     this.components.pagination.element.addEventListener('page-changed', e => {
       const pageIndex = e.detail;
 
-      const start = pageIndex * this.pageSize;
-      const end = start + this.pageSize;
-
-      this.components.cardsList.update(this.products.slice(start, end));
+      this.update(pageIndex + 1);
     });
+  }
+
+  async update(pageNumber) {
+    const data = await this.loadData(pageNumber);
+
+    this.components.cardsList.update(data);
+  }
+
+  async loadData(pageNumber) {
+    this.url.searchParams.set('_page', pageNumber);
+
+    const response = await fetch(this.url);
+    const products = await response.json();
+
+    return products;
   }
 }
 
-const page = new OnLineStorePage(products);
+const page = new OnLineStorePage();
 const root = document.querySelector('#root');
 
 root.append(page.element);
