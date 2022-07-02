c90072a45464c26e7fb498ed5d9ee4b5583d1615
Nazar Perih <nazar.perih@some.mail>
2022-07-02 13:27:56 +0300

    filters-list component implementation

--- src/components/filters-list/02-js-task/index.js
+++ src/components/filters-list/02-js-task/index.js
@@ -1,3 +1,68 @@
+const FILTERS_BLK_CSS_CLASS = "filters-panel-filters";
 export default class FiltersList {
+  #title = '';
+  #filters = null;
+  #element = null;
 
+  constructor ({title = '', list = null} = {}) {
+    this.#title = title;
+    this.#filters = list;
+  }
+
+  get element() {
+    if (!this.#element) {
+      this.#element = this.#render();
+    }
+    return this.#element;
+  }
+
+  #render() {
+    const tmp = document.createElement('div');
+    tmp.innerHTML = this.#getTemplate();
+    return tmp.firstElementChild;
+  }
+
+  #getTemplate() {
+    let sItemsHTML = '';
+    for (const filter of this.#filters) {
+      sItemsHTML += this.#getItemTemplate(filter);
+    }
+
+    return `
+            <form class="filters-panel">
+              <h3 class="filters-panel-title">${this.#title}</h3>
+              <div class=${FILTERS_BLK_CSS_CLASS}>
+                ${sItemsHTML}
+              </div>
+            </form>
+            `
+  }
+
+  #getItemTemplate({title = '', value = '', checked = false} = {}) {
+    const sChecked = checked ? ' checked' : '';
+    const result = `
+                    <div class="filters-panel-item">
+                      <label for="${value}">${title}</label>
+                      <input id="${value}" type="checkbox" name="filter" value="${value}"${sChecked}></input>
+                    </div>
+                    `
+
+    return result;
+  }
+
+  #getFiltersWrapper() {
+    const result = this.element ? this.element.querySelector(`.${FILTERS_BLK_CSS_CLASS}`) : null;
+
+    return result;
+  }
+
+  reset() {
+    const filtersWrapper = this.#getFiltersWrapper();
+    if (!filtersWrapper) return;
+
+    const checkboxes = filtersWrapper.querySelectorAll('[type = checkbox]');
+    for (const checkbox of checkboxes) {
+      checkbox.checked = false;
+    }
+  }
 }
