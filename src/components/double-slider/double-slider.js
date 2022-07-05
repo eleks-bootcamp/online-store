0a5e076e7079243b8cc8aee7511dcbe46c64426b
Valentyn <wolf.valentyn@gmail.com>
2022-07-05 11:11:42 +0300

    add class DoubleSlider, render elements, delete unnecessary files

--- src/components/double-slider/double-slider.js
+++ src/components/double-slider/double-slider.js
@@ -1,48 +1,99 @@
 'use strict';
 
-window.onload = function() {
-  renderSlider('slider-wrapper-1');
-  renderSlider('slider-wrapper-2');
-};
+export default class DoubleSlider {
+  constructor() {
+    this.renderPriceSlider();
+    this.renderRatingSlider();
+  }
 
+  getTeamplatePriceSlider () {
+    return `
+      <div class="slider__track"></div>
+      <input type="range" min="0" max="85000" value="0" class="slider__thumb slider-1">
+      <input type="range" min="0" max="85000" value="85000" class="slider__thumb slider__thumb_right slider-2">
+      <div class="slider__values">
+        <div class="slider__price-start">
+          <span class="range-1"></span>
+          UAH
+        </div>
+        <div class="slider__price-end">
+          <span class="range-2"></span>
+          UAH
+        </div>
+      </div>
+    `;
+  }
 
-const renderSlider = (wrapperId) => {
-  const parentElement = document.getElementById(wrapperId);
+  getTeamplateRatingSlider () {
+    return `
+      <div class="slider__track"></div>
+      <input type="range" min="0" max="5" value="0" step="0.01" class="slider__thumb slider-1">
+      <input type="range" min="0" max="5" value="5" step="0.01" class="slider__thumb slider__thumb_right slider-2">
+      <div class="slider__values">
+        <div class="slider__price-start">
+          <span class="range-1"></span>
+        </div>
+        <div class="slider__price-end">
+          <span class="range-2"></span>
+        </div>
+      </div>
+    `;
+  }
 
-  let sliderOne = parentElement.getElementsByClassName('slider-1')[0];
-  let sliderTwo = parentElement.getElementsByClassName('slider-2')[0];
+  renderPriceSlider () {
+    const priceElement = document.createElement('div');
 
-  sliderOne.addEventListener('input', slideOne);
-  sliderTwo.addEventListener('input', slideTwo);
+    priceElement.innerHTML = this.getTeamplatePriceSlider();
 
-  let displayValOne = parentElement.getElementsByClassName('range-1')[0];
-  let displayValTwo = parentElement.getElementsByClassName('range-2')[0];
-  let minGap = 0;
-  let sliderTrack = parentElement.querySelector('.slider__track');
-  let sliderMaxValue = parentElement.getElementsByClassName('slider-1')[0].max;
+    this.priceElement = priceElement;
+  }
 
-  function slideOne() {
-    if(parseFloat(sliderTwo.value) - parseFloat(sliderOne.value) <= minGap) {
-      sliderOne.value = parseFloat(sliderTwo.value) - minGap;
+  renderRatingSlider () {
+    const ratingElement = document.createElement('div');
+
+    ratingElement.innerHTML = this.getTeamplateRatingSlider();
+
+    this.ratingElement = ratingElement;
+  }
+
+  renderSlider(wrapperId) {
+    const parentElement = document.getElementById(wrapperId);
+
+    let sliderOne = parentElement.getElementsByClassName('slider-1')[0];
+    let sliderTwo = parentElement.getElementsByClassName('slider-2')[0];
+
+    sliderOne.addEventListener('input', slideOne);
+    sliderTwo.addEventListener('input', slideTwo);
+
+    let displayValOne = parentElement.getElementsByClassName('range-1')[0];
+    let displayValTwo = parentElement.getElementsByClassName('range-2')[0];
+    let minGap = 0;
+    let sliderTrack = parentElement.querySelector('.slider__track');
+    let sliderMaxValue = parentElement.getElementsByClassName('slider-1')[0].max;
+
+    function slideOne() {
+      if(parseFloat(sliderTwo.value) - parseFloat(sliderOne.value) <= minGap) {
+        sliderOne.value = parseFloat(sliderTwo.value) - minGap;
+      }
+      displayValOne.textContent = sliderOne.value;
+      fillColor();
     }
-    displayValOne.textContent = sliderOne.value;
-    fillColor();
-  }
 
-  function slideTwo() {
-    if(parseFloat(sliderTwo.value) - parseFloat(sliderOne.value) <= minGap) {
-      sliderTwo.value = parseFloat(sliderOne.value) + minGap;
+    function slideTwo() {
+      if(parseFloat(sliderTwo.value) - parseFloat(sliderOne.value) <= minGap) {
+        sliderTwo.value = parseFloat(sliderOne.value) + minGap;
+      }
+      displayValTwo.textContent = sliderTwo.value;
+      fillColor();
     }
-    displayValTwo.textContent = sliderTwo.value;
-    fillColor();
-  }
 
-  function fillColor() {
-    let percent1 = (sliderOne.value / sliderMaxValue) * 100;
-    let percent2 = (sliderTwo.value / sliderMaxValue) * 100;
-    sliderTrack.style.background = `linear-gradient(to right, #c8c2f3 ${percent1}%, #7E72F2 ${percent1}%, #7E72F2 ${percent2}%, #c8c2f3 ${percent2}%)`;
-  }
+    function fillColor() {
+      let percent1 = (sliderOne.value / sliderMaxValue) * 100;
+      let percent2 = (sliderTwo.value / sliderMaxValue) * 100;
+      sliderTrack.style.background = `linear-gradient(to right, #c8c2f3 ${percent1}%, #7E72F2 ${percent1}%, #7E72F2 ${percent2}%, #c8c2f3 ${percent2}%)`;
+    }
 
-  slideOne();
-  slideTwo();
-};
+    slideOne();
+    slideTwo();
+  }
+}
