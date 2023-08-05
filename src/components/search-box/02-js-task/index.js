ac13e1d2f0406711ea562a8e4d0766af1d1995a6
SDubytskiy <85970406+SDubytskiy@users.noreply.github.com>
2023-08-05 16:15:30 +0200

    Start to resolve problems
    came back

--- src/components/search-box/02-js-task/index.js
+++ src/components/search-box/02-js-task/index.js
@@ -1,3 +1,22 @@
 export default class Search {
+    constructor(){
+        this.myRender()
+    }
 
+    getTemplate(){
+        return`
+            <!-- SearchBox component -->
+            <form class="wert" action="/action_page.php">
+                <input type="text" class="search-box" placeholder="Search" name="search">
+            </form>`
+        }
+
+    myRender () {
+        const wrapper = document.createElement('div');
+        
+        wrapper.innerHTML = this.getTemplate();
+        
+        this.element = wrapper.firstElementChild;
+    }
+    
 }
--- src/components/side-bar/02-js-task/index.js
+++ src/components/side-bar/02-js-task/index.js
@@ -1,3 +1,131 @@
 export default class SideBar {
+    constructor () {
+        this.render()
+        this.initEventListeners()
+        this.showMyFunction()
+    }
 
+    getTemplate () {
+        return `<div class="checkbox-wrapper">
+        <!-- SideBar component -->
+        <div class="group-wrapper">
+            <h2>Category</h2>
+            <label for="monitors" data-element="monitor-filter" class="container">Monitors
+              <input type="checkbox" name="monitors" value="yes" id="monitors"  >
+              <span class="checkmark" ></span>
+            </label>
+            <label class="container">Laptops
+              <input type="checkbox" class="filter_value" name="laptops">
+              <span class="checkmark"></span>
+            </label>
+            <label class="container">Video cards
+              <input type="checkbox" class="filter_value">
+              <span class="checkmark"></span>
+            </label>
+            <label class="container">Gaming keyboards
+              <input type="checkbox" class="filter_value">
+              <span class="checkmark"></span>
+            </label>
+            <label class="container">Computer mouse
+              <input type="checkbox" >
+              <span class="checkmark"></span>
+            </label>
+            <label class="container">SSD
+              <input type="checkbox" class="filter_value">
+              <span class="checkmark"></span>
+            </label>
+            <label class="container">Sound cards
+              <input type="checkbox" class="filter_value">
+              <span class="checkmark"></span>
+            </label>
+            <label class="container">RAM
+              <input type="checkbox" class="filter_value">
+              <span class="checkmark"></span>
+            </label>
+          </div>
+          <div class="group-wrapper">
+            <h2>Brands</h2>
+            <label class="container">Asus
+              <input type="checkbox" >
+              <span class="checkmark"></span>
+            </label>
+            <label class="container">Acer
+              <input type="checkbox" class="filter_value">
+              <span class="checkmark"></span>
+            </label>
+            <label class="container">Apple
+              <input type="checkbox" class="filter_value">
+              <span class="checkmark"></span>
+            </label>
+            <label class="container">Dell
+              <input type="checkbox" class="filter_value">
+              <span class="checkmark"></span>
+            </label>
+            <label class="container">Dynamode
+              <input type="checkbox" >
+              <span class="checkmark"></span>
+            </label>
+            <label class="container">Gigabyte
+              <input type="checkbox" class="filter_value">
+              <span class="checkmark"></span>
+            </label>
+            <label class="container">Kingston
+              <input type="checkbox" class="filter_value">
+              <span class="checkmark"></span>
+            </label>
+            <label class="container">Lenovo
+              <input type="checkbox" class="filter_value">
+              <span class="checkmark"></span>
+            </label>
+            <label class="container">Logitech
+              <input type="checkbox" >
+              <span class="checkmark"></span>
+            </label>
+            <label class="container">MSI
+              <input type="checkbox" class="filter_value">
+              <span class="checkmark"></span>
+            </label>
+            <label class="container">BenQ
+              <input type="checkbox" class="filter_value">
+              <span class="checkmark"></span>
+            </label>
+            <label class="container">A4Tech
+              <input type="checkbox" class="filter_value">
+              <span class="checkmark"></span>
+            </label>
+          </div> 
+    
+        
+      </div>`
+    };
+
+    render () {
+        const wrapper = document.createElement('div');
+        
+        wrapper.innerHTML = this.getTemplate();
+        
+        this.element = wrapper.firstElementChild;
+    };
+
+    
+
+    initEventListeners(){
+      
+       const checkboxes = this.element.querySelector('#monitors');
+        checkboxes.addEventListener('click' , () => {
+            if(checkboxes.checked == true) {
+                console.log(checkboxes.name)
+            } else {
+                console.log(this.element.name)
+            }
+        });
+    }
+
+
+    showMyFunction(){
+        console.log()
+        
+    }
+
+    
 }
